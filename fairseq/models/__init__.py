32bf1dcb5e8d6eed8bd30de37a3f4e233acf9d94
Charles Hinson <cehinso17@gmail.com>
2019-05-14 11:52:42 +0800

    added the two new classes to __init__.py

--- fairseq/models/__init__.py
+++ fairseq/models/__init__.py
@@ -9,6 +9,8 @@ import argparse
 import importlib
 import os
 
+from .extra_feat_encoder import ExtraFeatEncoder
+from .extra_feat_decoder import ExtraFeatDecoder
 from .fairseq_decoder import FairseqDecoder  # noqa: F401
 from .fairseq_encoder import FairseqEncoder  # noqa: F401
 from .fairseq_incremental_decoder import FairseqIncrementalDecoder  # noqa: F401
@@ -55,9 +57,11 @@ def register_model(name):
 
     def register_model_cls(cls):
         if name in MODEL_REGISTRY:
-            raise ValueError('Cannot register duplicate model ({})'.format(name))
+            raise ValueError(
+                'Cannot register duplicate model ({})'.format(name))
         if not issubclass(cls, BaseFairseqModel):
-            raise ValueError('Model ({}: {}) must extend BaseFairseqModel'.format(name, cls.__name__))
+            raise ValueError(
+                'Model ({}: {}) must extend BaseFairseqModel'.format(name, cls.__name__))
         MODEL_REGISTRY[name] = cls
         return cls
 
@@ -91,11 +95,14 @@ def register_model_architecture(model_name, arch_name):
 
     def register_model_arch_fn(fn):
         if model_name not in MODEL_REGISTRY:
-            raise ValueError('Cannot register model architecture for unknown model type ({})'.format(model_name))
+            raise ValueError(
+                'Cannot register model architecture for unknown model type ({})'.format(model_name))
         if arch_name in ARCH_MODEL_REGISTRY:
-            raise ValueError('Cannot register duplicate model architecture ({})'.format(arch_name))
+            raise ValueError(
+                'Cannot register duplicate model architecture ({})'.format(arch_name))
         if not callable(fn):
-            raise ValueError('Model architecture must be callable ({})'.format(arch_name))
+            raise ValueError(
+                'Model architecture must be callable ({})'.format(arch_name))
         ARCH_MODEL_REGISTRY[arch_name] = MODEL_REGISTRY[model_name]
         ARCH_MODEL_INV_REGISTRY.setdefault(model_name, []).append(arch_name)
         ARCH_CONFIG_REGISTRY[arch_name] = fn
@@ -114,7 +121,9 @@ for file in os.listdir(os.path.dirname(__file__)):
         if model_name in MODEL_REGISTRY:
             parser = argparse.ArgumentParser(add_help=False)
             group_archs = parser.add_argument_group('Named architectures')
-            group_archs.add_argument('--arch', choices=ARCH_MODEL_INV_REGISTRY[model_name])
-            group_args = parser.add_argument_group('Additional command-line arguments')
+            group_archs.add_argument(
+                '--arch', choices=ARCH_MODEL_INV_REGISTRY[model_name])
+            group_args = parser.add_argument_group(
+                'Additional command-line arguments')
             MODEL_REGISTRY[model_name].add_args(group_args)
             globals()[model_name + '_parser'] = parser
