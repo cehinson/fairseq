24a052794e564d7667bde454750741e9679816c8
Charles Hinson <cehinso17@gmail.com>
2019-05-21 15:03:16 +0800

    added bert_src and bert_tgt

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -72,11 +72,15 @@ class TranslationTask(FairseqTask):
         # fmt: on
         parser.add_argument('--use_bert', default='False', type=str,
                             metavar='BOOL', help='use bert feature embeddings')
+        parser.add_argument('--bert_data',
+                            help='path to data that has NOT had BPE applied to it')
 
-    def __init__(self, args, src_dict, tgt_dict):
+    def __init__(self, args, src_dict, tgt_dict, bert_src_dict=None, bert_tgt_dict=None):
         super().__init__(args)
         self.src_dict = src_dict
         self.tgt_dict = tgt_dict
+        self.bert_src_dict = bert_src_dict
+        self.bert_tgt_dict = bert_tgt_dict
 
     @classmethod
     def setup_task(cls, args, **kwargs):
@@ -92,6 +96,9 @@ class TranslationTask(FairseqTask):
         if isinstance(args.data, str):
             args.data = [args.data]
 
+        if isinstance(args.bert_data, str):
+            args.bert_data = [args.bert_data]
+
         # find language pair automatically
         if args.source_lang is None or args.target_lang is None:
             args.source_lang, args.target_lang = data_utils.infer_language_pair(
@@ -99,6 +106,15 @@ class TranslationTask(FairseqTask):
         if args.source_lang is None or args.target_lang is None:
             raise Exception(
                 'Could not infer language pair, please provide it explicitly')
+
+        # find language pair automatically -- Bert data has NO BPE applied
+        if args.use_bert:
+            if args.source_lang is None or args.target_lang is None:
+                args.source_lang, args.target_lang = data_utils.infer_language_pair(
+                    args.bert_data[0])
+            if args.source_lang is None or args.target_lang is None:
+                raise Exception(
+                    'Could not infer bert_data language pair, please provide it explicitly')
 
         # load dictionaries
         src_dict = cls.load_dictionary(os.path.join(
@@ -113,7 +129,23 @@ class TranslationTask(FairseqTask):
         print('| [{}] dictionary: {} types'.format(
             args.target_lang, len(tgt_dict)))
 
-        return cls(args, src_dict, tgt_dict)
+        # load bert data dictionaries
+        bert_src_dict = None
+        bert_tgt_dict = None
+        if args.use_bert:
+            bert_src_dict = cls.load_dictionary(os.path.join(
+                args.bert_data[0], 'dict.{}.txt'.format(args.source_lang)))
+            bert_tgt_dict = cls.load_dictionary(os.path.join(
+                args.bert_data[0], 'dict.{}.txt'.format(args.target_lang)))
+            assert bert_src_dict.pad() == bert_tgt_dict.pad()
+            assert bert_src_dict.eos() == bert_tgt_dict.eos()
+            assert bert_src_dict.unk() == bert_tgt_dict.unk()
+            print('| [{}] bert dictionary: {} types'.format(
+                args.source_lang, len(bert_src_dict)))
+            print('| [{}] bert dictionary: {} types'.format(
+                args.target_lang, len(bert_tgt_dict)))
+
+        return cls(args, src_dict, tgt_dict, bert_src_dict, bert_tgt_dict)
 
     def load_dataset(self, split, combine=False, **kwargs):
         """Load a given dataset split.
@@ -176,18 +208,64 @@ class TranslationTask(FairseqTask):
 
         assert len(src_datasets) == len(tgt_datasets)
 
+        if self.args.use_bert:
+            bert_src_datasets = []
+            bert_tgt_datasets = []
+
+        for dk, data_path in enumerate(self.args.bert_data):
+            for k in itertools.count():
+                split_k = split + (str(k) if k > 0 else '')
+
+                # infer langcode
+                src, tgt = self.args.source_lang, self.args.target_lang
+                if split_exists(split_k, src, tgt, src, data_path):
+                    prefix = os.path.join(
+                        data_path, '{}.{}-{}.'.format(split_k, src, tgt))
+                elif split_exists(split_k, tgt, src, src, data_path):
+                    prefix = os.path.join(
+                        data_path, '{}.{}-{}.'.format(split_k, tgt, src))
+                else:
+                    if k > 0 or dk > 0:
+                        break
+                    else:
+                        raise FileNotFoundError(
+                            'Bert Dataset not found: {} ({})'.format(split, data_path))
+
+                bert_src_datasets.append(indexed_dataset(
+                    prefix + src, self.bert_src_dict))
+                bert_tgt_datasets.append(indexed_dataset(
+                    prefix + tgt, self.bert_tgt_dict))
+
+                print('| {} {} {} examples'.format(
+                    data_path, split_k, len(bert_src_datasets[-1])))
+
+                if not combine:
+                    break
+
+        assert len(bert_src_datasets) == len(bert_tgt_datasets)
+
         if len(src_datasets) == 1:
             src_dataset, tgt_dataset = src_datasets[0], tgt_datasets[0]
         else:
-            sample_ratios = [1] * len(src_datasets)
+            sample_ratios = [1] * len(bert_src_datasets)
             sample_ratios[0] = self.args.upsample_primary
             src_dataset = ConcatDataset(src_datasets, sample_ratios)
             tgt_dataset = ConcatDataset(tgt_datasets, sample_ratios)
+
+        if len(bert_src_datasets) == 1:
+            bert_src_dataset, bert_tgt_dataset = bert_src_datasets[0], bert_tgt_datasets[0]
+        else:
+            sample_ratios = [1] * len(src_datasets)
+            sample_ratios[0] = self.args.upsample_primary
+            bert_src_dataset = ConcatDataset(bert_src_datasets, sample_ratios)
+            bert_tgt_dataset = ConcatDataset(bert_tgt_datasets, sample_ratios)
 
         if self.args.use_bert:
             self.datasets[split] = LangPairBertFeatDataset(
                 src_dataset, src_dataset.sizes, self.src_dict,
                 tgt_dataset, tgt_dataset.sizes, self.tgt_dict,
+                bert_src_dataset, bert_src_dataset.sizes, self.bert_src_dict,
+                bert_tgt_dataset, bert_tgt_dataset.sizes, self.bert_tgt_dict,
                 left_pad_source=self.args.left_pad_source,
                 left_pad_target=self.args.left_pad_target,
                 max_source_positions=self.args.max_source_positions,
